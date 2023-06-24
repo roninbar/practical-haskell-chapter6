c51daeb3914904ecfd2ee12ae762ff1b2929d077
ron <rinbar70@gmail.com>
2023-06-24 22:49:24 +0300

    Implement the k-means algorithm.

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,14 @@
-{-# LANGUAGE LambdaCase      #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE InstanceSigs          #-}
+{-# LANGUAGE LambdaCase            #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell       #-}
 
 module Main where
 
+import           Control.Monad.State    (MonadState (get), State, gets, modify)
+import           Data.Foldable          (minimumBy)
+import qualified Data.Map               as M
 import           Lens.Micro             (Lens, Lens', lens, (^.))
 import           Lens.Micro.TH          (makeLenses)
 import           System.Posix.Internals (c_link)
@@ -72,8 +78,62 @@ makeLenses ''Client
 
 makeLenses ''Person
 
+class Ord v => Vector v where
+  distance :: v -> v -> Double
+  centroid :: [v] -> v
+
+instance Vector (Double, Double) where
+  distance :: (Double, Double) -> (Double, Double) -> Double
+  distance (a, b) (c, d) = sqrt $ (c - a) ^ 2 + (d - b) ^ 2
+  centroid :: [(Double, Double)] -> (Double, Double)
+  centroid vs =
+    let (u, v) = foldr (\(a, b) (c, d) -> (a + c, b + d)) (0, 0) vs
+        n = fromIntegral $ length vs
+     in (u / n, v / n)
+
+class Vector v =>
+      Vectorizable e v
+  where
+  toVector :: e -> v
+
+data KMeansState v =
+  KMeansState
+    { centroids :: [v]
+    , threshold :: Double
+    , steps     :: Int
+    }
+
+newCentroids :: (Vector v, Vectorizable e v) => M.Map v [e] -> [v]
+newCentroids = M.elems . fmap (centroid . map toVector)
+
+clusterAssignments :: (Vector v, Vectorizable e v) => [v] -> [e] -> M.Map v [e]
+clusterAssignments centrs points =
+  let initialMap = M.fromList $ zip centrs (repeat [])
+   in foldr
+        (\p m ->
+           let chosenC = minimumBy (compareDistance p) centrs
+            in M.adjust (p :) chosenC m)
+        initialMap
+        points
+  where
+    compareDistance p x y =
+      compare (distance x $ toVector p) (distance y $ toVector p)
+
+kMeans' :: (Vector v, Vectorizable e v) => [e] -> State (KMeansState v) [v]
+kMeans' points = do
+  prevCentrs <- gets centroids
+  let assignments = clusterAssignments prevCentrs points
+      newCentrs = newCentroids assignments
+  modify (\s -> s {centroids = newCentrs})
+  modify (\s -> s {steps = steps s + 1})
+  t <- gets threshold
+  let err = sum $ zipWith distance prevCentrs newCentrs
+  if err < t
+    then return newCentrs
+    else kMeans' points
+
 main :: IO ()
 main =
   let p = Person "John" "Smith"
       client = Individual 3 p
-   in print $ client ^. person . fullName
+   in print $ client ^. person . firstName
